ab7ae5d0b9a679155ffdd722e688cb9d68bd075e
Peter-Benedikt von Niebelschütz <fogshot@gmail.com>
2018-11-07 15:32:25 +0100

    add Representative stub

---
+++ src/common/Representative.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Peter-Benedikt von Niebelschütz on 07.11.18.
+//
+
+#include "Representative.h"
---
+++ src/common/Representative.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter-Benedikt von Niebelschütz on 07.11.18.
+//
+
+#ifndef LLVM_REPRESENTATIVE_H
+#define LLVM_REPRESENTATIVE_H
+
+
+class Representative {
+
+};
+
+
+#endif //LLVM_REPRESENTATIVE_H
