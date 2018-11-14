465b4c5c305194748d51246f144022938a565188
Dominik Horn <horn.dominik101@yahoo.de>
2018-11-14 11:52:45 +0100

    McFix

--- src/common/InstructionVisitor.cpp
+++ src/common/InstructionVisitor.cpp
@@ -24,8 +24,8 @@ void InstructionVisitor::visit(BasicBlock &bb) {
 
 void InstructionVisitor::visit(Instruction &inst) {
     // TODO: debug output that should be removed (just to have some sort of indication for missing instruction visit hooks)
-    DEBUG_OUTPUT(std::string(YELLOW)
-                         +"inst(" + std::string(inst.getOpcodeName()) + ")" + std::string(NO_COLOR));
+//    DEBUG_OUTPUT(std::string(YELLOW)
+//                         +"inst(" + std::string(inst.getOpcodeName()) + ")" + std::string(NO_COLOR));
 
     globalDebugOutputTabLevel++;
     InstVisitor::visit(inst);
