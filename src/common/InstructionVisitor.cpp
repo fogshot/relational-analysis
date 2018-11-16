6219f16caefea3da75d303a45cc7a46322a717eb
Dominik Horn <horn.dominik101@yahoo.de>
2018-11-16 10:45:38 +0100

    Update Code in InstructionVisitor
    * Better debug output

--- src/common/InstructionVisitor.cpp
+++ src/common/InstructionVisitor.cpp
@@ -21,11 +21,14 @@ InstructionVisitor::InstructionVisitor(std::shared_ptr<AbstractDomain> startDoma
 void InstructionVisitor::visit(BasicBlock &bb) {
     DEBUG_OUTPUT(std::string(GREEN)
                          +"Visiting \"" + bb.getName().str() + "\"" + std::string(NO_COLOR));
-    state->willVisit();
+    DEBUG_OUTPUT(std::string(GREEN) + "State before: " + state->toString() + std::string(NO_COLOR));
 
     globalDebugOutputTabLevel++;
+    state->willVisit();
     InstVisitor::visit(bb);
     globalDebugOutputTabLevel--;
+
+    DEBUG_OUTPUT(std::string(GREEN) + "State after: " + state->toString() + std::string(NO_COLOR));
 }
 
 void InstructionVisitor::visit(Instruction &inst) {
@@ -81,17 +84,15 @@ void InstructionVisitor::visitAdd(BinaryOperator &inst) {
     std::shared_ptr<Variable> destination = helperParseVariable(&inst);
     std::shared_ptr<Representative> arg1 = helperParseOperand(inst.getOperand(0));
     std::shared_ptr<Representative> arg2 = helperParseOperand(inst.getOperand(1));
+    DEBUG_OUTPUT(
+            "-> transform_add(" + destination->toString() + ", " + arg1->toString() + ", " + arg2->toString() + ")");
 
+    // TODO generify this code since its the same for all visit* impls
     auto domains = state->getDomains();
     for (auto domIt = domains.begin(); domIt < domains.end(); domIt++) {
-        auto domain = domIt->get();
-        DEBUG_OUTPUT(
-                "-> transform_add(" + destination->toString() + ", " + arg1->toString() + ", " + arg2->toString() +
-                ")");
-        if (domain->transform_add(destination, arg1, arg2)) {
+        if (domIt->get()->transform_add(destination, arg1, arg2)) {
             state->setUpdated();
         }
-        DEBUG_OUTPUT("Domain: " + domain->toString());
     }
 }
 
@@ -103,16 +104,14 @@ void InstructionVisitor::visitStoreInst(StoreInst &inst) {
 
     std::shared_ptr<Variable> destination = helperParseVariable(inst.getOperand(1));
     std::shared_ptr<Representative> arg1 = helperParseOperand(inst.getOperand(0));
+    DEBUG_OUTPUT("-> transform_store(" + destination->toString() + ", " + arg1->toString() + ")");
 
+    // TODO generify this code since its the same for all visit* impls
     auto domains = state->getDomains();
     for (auto domIt = domains.begin(); domIt < domains.end(); domIt++) {
-        auto domain = domIt->get();
-        // Send store to every domain
-        DEBUG_OUTPUT("-> transform_store(" + destination->toString() + ", " + arg1->toString() + ")");
-        if (domain->transform_store(destination, arg1)) {
+        if (domIt->get()->transform_store(destination, arg1)) {
             state->setUpdated();
         }
-        DEBUG_OUTPUT("Domain: " + domain->toString());
     }
 }
 
@@ -123,15 +122,14 @@ void InstructionVisitor::visitLoadInst(LoadInst &inst) {
 
     std::shared_ptr<Variable> destination = helperParseVariable(&inst);
     std::shared_ptr<Representative> arg1 = helperParseOperand(inst.getOperand(0));
+    DEBUG_OUTPUT("-> transform_load(" + destination->toString() + ", " + arg1->toString() + ")");
 
+    // TODO generify this code since its the same for all visit* impls
     auto domains = state->getDomains();
     for (auto domIt = domains.begin(); domIt < domains.end(); domIt++) {
-        auto domain = domIt->get();
-        DEBUG_OUTPUT("-> transform_load(" + destination->toString() + ", " + arg1->toString() + ")");
-        if (domain->transform_load(destination, arg1)) {
+        if (domIt->get()->transform_load(destination, arg1)) {
             state->setUpdated();
         }
-        DEBUG_OUTPUT("Domain: " + domain->toString());
     }
 }
 
