7e0225c5544ce3e0f25d7731a760cbb6f2251b95
Dominik Horn <horn.dominik101@yahoo.de>
2018-11-16 14:48:40 +0100

    Add -m option to cfg gen (preopt with mem2reg)

--- samples/generateCFG.sh
+++ samples/generateCFG.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+while :; do
+    case $1 in
+        -m|--mem2reg)
+        optFlag1="mem2reg"
+        ;;
+        *) break
+    esac
+    shift
+done
+
 # if one argument passed: only analyze the passed program
 if [ $# == 1 ] ; then
     ARRAY=($1)
 else # run all samples
     ARRAY=($(ls -d *.c))
 fi
-
 
 # if no folder is existent
 mkdir -p build
@@ -24,17 +33,20 @@ do
     printf "${BLUE} Compiling to llvm IR using clang... ${NC}\n"
     $LLVM_BUILD_PATH/bin/clang -O0 -emit-llvm $f -Xclang -disable-O0-optnone -c -o build/$f.bc
 
-	# NO OPTIMIZATION!!
-    # ... run mem2reg optimization
-    printf "${BLUE} Optimizing using mem2reg... ${NC}\n"
-    $LLVM_BUILD_PATH/bin/opt -mem2reg < build/$f.bc > build/$f-opt.bc
-    # ... disassemble optimized file
-    printf "${BLUE} Disassemble optimized file... ${NC}\n"
-    $LLVM_BUILD_PATH/bin/llvm-dis build/$f-opt.bc
+    if  [ "$optFlag1" = "mem2reg" ]; then
+        # ... run mem2reg optimization
+        printf "${BLUE} Optimizing using mem2reg... ${NC}\n"
+        $LLVM_BUILD_PATH/bin/opt -mem2reg < build/$f.bc > build/$f-opt.bc
+        # ... disassemble optimized file
+        printf "${BLUE} Disassemble optimized file... ${NC}\n"
+        $LLVM_BUILD_PATH/bin/llvm-dis build/$f-opt.bc
+        # generate .dot from CFG
+        $LLVM_BUILD_PATH/bin/opt -dot-cfg < build/$f-opt.bc > /dev/null
+    else
+        # generate .dot from CFG
+        $LLVM_BUILD_PATH/bin/opt -dot-cfg < build/$f.bc > /dev/null
+    fi
 
-    # generate .dot from CFG
-#    $LLVM_BUILD_PATH/bin/opt -dot-cfg < build/$f-opt.bc > /dev/null
-    $LLVM_BUILD_PATH/bin/opt -dot-cfg < build/$f.bc > /dev/null
     # convert .dot to .pdf and store under correct path
 	DOT_FILES=($(ls -d *.dot))
 	for dotfile in ${DOT_FILES[*]};
